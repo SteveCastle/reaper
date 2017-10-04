f04096fb3a3e2cd82a53e896c213e0b11ba171c1
Steve Castle <steve@tigerhaunt.com>
2017-10-03 20:16:49 -0700

    css playground

---
+++ playground/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Output from './output';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Output />
+    );
+  }
+}
+
+export default App;
